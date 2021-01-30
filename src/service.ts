478ffb6564f41406daeff68b2616c15c88299414
BryanCruz <bryan.bialokur@gmail.com>
2021-01-30 16:57:37 -0300

    feat: add a password checker interface

--- src/service.ts
+++ src/service.ts
@@ -1,3 +1,7 @@
-export const isPasswordValid = (password: string): boolean => {
-  return false;
+type passwordChecker = (password: string) => boolean;
+
+export const isPasswordValid: passwordChecker = (password) => {
+  return checkers.every((checkerFn) => checkerFn(password));
 };
+
+const checkers: passwordChecker[] = [];
